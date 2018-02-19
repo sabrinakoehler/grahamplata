16e327ed67d76bf7ef94daee8d3106904d7161dc
Graham Plata <graham.plata@gmail.com>
2018-02-19 16:56:26 -0500

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = () => (
+    <footer>
+        <p>I'm the Sticky Footer.</p>
+    </footer>
+)
+
+export default Footer
+
