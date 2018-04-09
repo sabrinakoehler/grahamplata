3e31b05c1dea8373202c1dba2a36050f07435582
Graham Plata <graham.plata@gmail.com>
2018-04-08 21:49:43 -0400

    project page placeholder

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Link from "gatsby-link";
-import { Container, Header } from "semantic-ui-react";
+import { Grid, Header } from "semantic-ui-react";
 
-const Projects = () => (
-  <Container fluid>
-    <Header as="h1">Projects</Header>
-  </Container>
+const styles = {
+  spacing: {
+    paddingTop: "3em",
+    paddingBottom: "3em"
+  }
+};
+
+const Projects = ({ data }) => (
+  <div style={styles.spacing}>
+    <Grid stackable columns={2}>
+      <Grid.Column>
+        <h2>Projects</h2>
+        <h3>{"<Coming soon />"}</h3>
+      </Grid.Column>
+      <Grid.Column />
+    </Grid>
+  </div>
 );
 
 export default Projects;
