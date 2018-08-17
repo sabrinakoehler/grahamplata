168202fc239ac7385d88bdcea0d47930bbeeff45
Graham Plata <graham.plata@gmail.com>
2018-08-17 12:28:22 -0400

    Change query to use StaticQuery

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,68 +4,72 @@ import { Card, Image, Grid, Icon } from "semantic-ui-react";
 import Layout from "../components/layout";
 import { StaticQuery, graphql } from "gatsby";
 
-const BlogPage = ({ data, props }) => (
-  <Layout location={props.location}>
-    <Grid stackable columns={3}>
-      {data.allMarkdownRemark.edges.map(post => (
-        <Grid.Column>
-          <Card fluid key={post.node.id}>
-            <Image
-              fluid
-              src={
-                post.node.frontmatter.featuredImage.childImageSharp.sizes.src
-              }
-            />
-            <Card.Content>
-              <Card.Header>
-                <Link to={post.node.frontmatter.path}>
-                  {post.node.frontmatter.title}
-                </Link>
-              </Card.Header>
-              <Card.Meta>
-                <span className="date">{post.node.frontmatter.date}</span>
-              </Card.Meta>
-              <Card.Description>{post.node.excerpt}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a href="https://twitter.com/grahamplata">
-                <Icon name="user" />
-                @grahamplata
-              </a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-      ))}
-    </Grid>
-  </Layout>
-);
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 10
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 100)
-          id
-          frontmatter {
-            title
-            path
-            date(formatString: "MMMM DD, YYYY")
-            featuredImage {
-              childImageSharp {
-                sizes(maxWidth: 550) {
-                  ...GatsbyImageSharpSizes
+const BlogPage = ({ children, props }) => (
+  <StaticQuery
+    query={graphql`
+      query IndexQuery {
+        allMarkdownRemark(
+          limit: 10
+          sort: { fields: [frontmatter___date], order: DESC }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 100)
+              id
+              frontmatter {
+                title
+                path
+                date(formatString: "MMMM DD, YYYY")
+                featuredImage {
+                  childImageSharp {
+                    sizes(maxWidth: 550) {
+                      ...GatsbyImageSharpSizes
+                    }
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  }
-`;
+    `}
+    render={data => (
+      <Layout>
+        <Grid stackable columns={3}>
+          {data.allMarkdownRemark.edges.map(post => (
+            <Grid.Column>
+              <Card fluid key={post.node.id}>
+                <Image
+                  fluid
+                  src={
+                    post.node.frontmatter.featuredImage.childImageSharp.sizes
+                      .src
+                  }
+                />
+                <Card.Content>
+                  <Card.Header>
+                    <Link to={post.node.frontmatter.path}>
+                      {post.node.frontmatter.title}
+                    </Link>
+                  </Card.Header>
+                  <Card.Meta>
+                    <span className="date">{post.node.frontmatter.date}</span>
+                  </Card.Meta>
+                  <Card.Description>{post.node.excerpt}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a href="https://twitter.com/grahamplata">
+                    <Icon name="user" />
+                    @grahamplata
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          ))}
+        </Grid>
+      </Layout>
+    )}
+  />
+);
 
 export default BlogPage;
